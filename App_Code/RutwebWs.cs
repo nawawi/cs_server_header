375e2a42ff70f221c35ee240faff151910e8ee76
Nawawi Jamili <nawawi@rutweb.com>
2015-08-24 15:46:12 +0800

    Create RutwebWs.cs

---
+++ App_Code/RutwebWs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+
+/*
+Add this configuration at web.config
+<modules>
+    <add name="ServerHeader" type="RutwebWs.ServerHeader" />
+</modules>
+*/
+
+namespace RutwebWs {
+	public class ServerHeader : IHttpModule {
+		public void Init( HttpApplication context ) {
+			context.PreSendRequestHeaders += OnPreSendRequestHeaders;
+		}
+
+		public void Dispose() { }
+
+		private void OnPreSendRequestHeaders( object sender, EventArgs e ) {
+			HttpContext.Current.Response.Headers.Set( "Server", "rutweb/WS -- http://rutweb.com" );
+		}
+	}
+}
